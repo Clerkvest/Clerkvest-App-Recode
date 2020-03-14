8ca0ca29d3b892b89e55d97151cf97afe9d3c9bf
Qu1oX <danny1996@gmx.net>
2020-03-14 12:15:48 +0100

    Added unstyled error pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
+import { NotFoundComponent } from './execption/pages/not-found/not-found.component';
+import { UnauthorizedComponent } from './execption/pages/unauthorized/unauthorized.component';
+import { InternalServerErrorComponent } from './execption/pages/internal-server-error/internal-server-error.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'shared-overview', component: null}
+  {path: '500', component: InternalServerErrorComponent},
+  {path: '401', component: UnauthorizedComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InternalServerErrorComponent } from './execption/pages/internal-server-error/internal-server-error.component';
+import { NotFoundComponent } from './execption/pages/not-found/not-found.component';
+import { UnauthorizedComponent } from './execption/pages/unauthorized/unauthorized.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InternalServerErrorComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
