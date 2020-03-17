365f1b5c7bbe9e0d492ab4610d1d002d6867de08
Qu1oX <danny1996@gmx.net>
2020-03-17 13:10:18 +0100

    Added company endpoints

--- src/app/core/api/repository/company/company-repository.service.spec.ts
+++ src/app/core/api/repository/company/company-repository.service.spec.ts
@@ -1,16 +1,73 @@
+import { ICompany } from './../../models/ICompany';
 import { TestBed } from '@angular/core/testing';
 
 import { CompanyRepositoryService } from './company-repository.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CompanyRepositoryService', () => {
   let service: CompanyRepositoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientModule]});
     service = TestBed.inject(CompanyRepositoryService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return a ICompany object', (done) => {
+    let company$ = service.getById(0);
+
+    company$.subscribe(
+      company => {
+        expect(company).toBeDefined();
+        expect(company.id).toBeDefined();
+        expect(company.name).toBeDefined();
+        expect(company.domain).toBeDefined();
+        expect(company.image).toBeDefined();
+        expect(company.payAmount).toBeDefined();
+        expect(company.payInterval).toBeDefined();
+        expect(company.inviteOnly).toBeDefined();
+        done();
+      },
+      error => {
+        expect(error).not.toBeDefined();
+        done();
+      }
+    )
+  });
+
+  it('should update and return a ICompany object', (done) => {
+
+    let _company: ICompany = new class implements ICompany {
+      id?: number;
+      name?: string;
+      domain?: string;
+      image?: number;
+      payAmount?: number;
+      payInterval?: number;
+      inviteOnly?: boolean;
+    }
+
+    let company$ = service.update(_company);
+
+    company$.subscribe(
+      company => {
+        expect(company).toBeDefined();
+        expect(company.id).toBeDefined();
+        expect(company.name).toBeDefined();
+        expect(company.domain).toBeDefined();
+        expect(company.image).toBeDefined();
+        expect(company.payAmount).toBeDefined();
+        expect(company.payInterval).toBeDefined();
+        expect(company.inviteOnly).toBeDefined();
+        done();
+      },
+      error => {
+        expect(error).not.toBeDefined();
+        done();
+      }
+    )
   });
 });
--- src/app/core/api/repository/company/company-repository.service.ts
+++ src/app/core/api/repository/company/company-repository.service.ts
@@ -1,9 +1,47 @@
+import { ICompany } from './../../models/ICompany';
 import { Injectable } from '@angular/core';
+import { LoggerService } from 'src/app/core/logger/logger.service';
+import { ConfigurationService } from '../configuration.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyRepositoryService {
 
-  constructor() { }
+  public static readonly SERVICE_BASE: string = '/company/'
+
+  private static readonly GET_SINGLE: string = CompanyRepositoryService.SERVICE_BASE + 'get/';
+  private static readonly UPDATE: string = CompanyRepositoryService.SERVICE_BASE + 'update';
+
+  private logger: LoggerService = LoggerService.build(CompanyRepositoryService.name);
+
+  private basePath: string;
+
+  constructor(private configuration: ConfigurationService, private httpClient: HttpClient) {
+    this.basePath = configuration.getBasePath();
+  }
+
+  public getById(id: number): Observable<ICompany> {
+    let headers = this.configuration.getHeaders();
+    let url = this.basePath + CompanyRepositoryService.GET_SINGLE + encodeURIComponent(String(id));
+    this.logger.debug('Executing request: ' + url);
+
+    return this.httpClient.get<ICompany>(url,
+    {
+        headers: headers,
+    });
+  }
+
+  public update(body: ICompany) {
+    let headers = this.configuration.getHeaders();
+    let url = this.basePath + CompanyRepositoryService.UPDATE;
+    this.logger.debug('Executing request: ' + url);
+
+    return this.httpClient.put<ICompany>(url, body,
+    {
+        headers: headers,
+    });
+  }
 }
