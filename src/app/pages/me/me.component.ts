6ad966bcf1b8e6cd446d64e52950c456fba721dc
Michael Keller <m.keller490@gmail.com>
2020-07-15 12:40:55 +0200

    Added Profile Design -Edit Profile -Notification Preferences -Non existing Projects/ Investments

--- src/app/pages/me/me.component.ts
+++ src/app/pages/me/me.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserRepositoryService} from 'src/app/core/api/repository/user/user-repository.service';
+import {LoggerService} from '../../core/utils/logger/logger.service';
+import {SubscriptionService} from '../../core/utils/subscription/subscription.service';
+import {IEmployee} from '../../core/api/models/IEmployee';
 
 @Component({
   selector: 'app-me',
   templateUrl: './me.component.html',
   styleUrls: ['./me.component.scss']
 })
-export class MeComponent implements OnInit {
+export class MeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _logger: LoggerService;
+  private _subscriptionService: SubscriptionService;
+  private _userRepo: UserRepositoryService;
 
-  ngOnInit(): void {
+  constructor(private _userRepository: UserRepositoryService) {
+    this._userRepo = _userRepository;
+  }
+
+  private _userObj: IEmployee;
+
+  get userObj(): IEmployee {
+    return this._userObj;
+  }
+
+  get myProjects(): [] {
+    return [];
   }
 
   /**
@@ -18,5 +35,38 @@ export class MeComponent implements OnInit {
    */
   public get hasProfileImage(): boolean {
     return false;
+  }
+
+  get myInvestments(): [] {
+    return [];
+  }
+
+  ngOnInit(): void {
+    this._logger = LoggerService.build(MeComponent.name);
+    this._subscriptionService = new SubscriptionService();
+    //this._userRepo.getMyself().toPromise().then(loadUserDetails(value))
+
+    this._subscriptionService.add(this._userRepo.getMyself().subscribe(
+      response => {
+        this._userObj = response;
+      },
+      err => {
+
+      },
+      () => {
+
+      }
+    ), 'MeComponent#ngOnInit');
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptionService.unsubscribe();
+  }
+
+  saveProfileChanges() {
+  }
+
+  saveNotificationChanges() {
+
   }
 }
